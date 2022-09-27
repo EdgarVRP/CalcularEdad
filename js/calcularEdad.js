bec6c61834c15c4cc840d1b54451d6a827bce7ce
EdgarVRP <edgarvrp22@gmail.com>
2022-09-27 13:32:23 -0500

    Se actualiza para contemplar dias y meses

--- js/calcularEdad.js
+++ js/calcularEdad.js
@@ -7,26 +7,28 @@
 var boton = document.getElementById("btn");
 
 function calcularEdad(fecha) {
-    console.log(document.getElementById("yearActual").value);
     if(document.getElementById("yearActual").value== ""){	
     var hoy = new Date();
     document.getElementById("yearActual").value = hoy.getFullYear();
-    document.getElementById("monthActual").value = hoy.getMonth() ;
+    document.getElementById("monthActual").value = hoy.getMonth()+1 ;
     document.getElementById("dayActual").value = hoy.getDate();
     }else{
-    var hoy = new Date(document.getElementById("yearActual").value,document.getElementById("monthActual").value,document.getElementById("dayActual").value);
+    var hoy = new Date(document.getElementById("yearActual").value,document.getElementById("monthActual").value-1,document.getElementById("dayActual").value);
     }
-    var year=fecha[0] + fecha[1] + fecha[2] + fecha[3];
-    var month=fecha[5] + fecha[6];
-    var day=fecha[8] + fecha[9];
-    
+    console.log(hoy);
+    var year=Number(fecha[0] + fecha[1] + fecha[2] + fecha[3]);
+    var month=Number(fecha[5] + fecha[6]);
+    var day=Number(fecha[8] + fecha[9]);
     var edad = hoy.getFullYear() - year;
-    console.log(edad);
     var m = hoy.getMonth() - month;
+    console.log(m < -1);
     console.log(m);
-    if (m < 0 || (m === 0 && hoy.getDate() < day)) {
+    console.log(hoy.getDate() < day);
+    console.log(edad);
+    if (m < -1 || (m == -1 && hoy.getDate() < day)) {
         edad--;
     }
+    console.log(edad);
     return edad;
 }
 boton.onclick = function () {
